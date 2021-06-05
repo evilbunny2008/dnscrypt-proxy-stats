76416bdfe81ca6c5852345a2465c9e8e04db092b
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2021-06-05 16:39:40 +1000

    index.php layout

--- index.php
+++ index.php
@@ -17,11 +17,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DNSCrypt-Proxy Stats</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.3.2/chart.min.js"></script>
+    <style class="INLINE_PEN_STYLESHEET_ID">
+html, body, .grid-container { height: 100%; margin: 0; }
+
+.grid-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 0px 0px;
+}
+
+.DNS-Hits { grid-area: 1 / 1 / 2 / 5; }
+
+.Queries { grid-area: 1 / 4 / 2 / 5; }
+
+.Total-Queries-by-Return-Code { grid-area: 2 / 1 / 3 / 2; }
+
+.Total-Queries-by-Server { grid-area: 2 / 2 / 3 / 3; }
+
+.Total-Queries-by-Src-IP { grid-area: 2 / 3 / 3 / 4; }
+
+.Total-Queries-by-Type { grid-area: 2 / 4 / 3 / 5; }
+    </style>
   </head>
   <body>
 
     <div>
-	<canvas id="myChart" width='400' height='100'></canvas>
     </div>
 <?php
 	$period = 600;
@@ -52,6 +73,50 @@
 	}
 ?>
 
+<div class="grid-container">
+  <div class="DNS-Hits" style='width: 100%;height: 100%;'><canvas id="myChart" style='width: 75% !important;height: 100% !important;'></canvas></div>
+  <div class="Queries">
+<?php
+	$query = "SELECT count(`time`) as `count` FROM `ltsv` where `time` >= $startTime and `time` < $now + $period";
+	$res = mysqli_query($link, $query);
+	$queries = mysqli_fetch_assoc($res)['count'];
+	echo "$queries Queries";
+?>
+  </div>
+  <div class="Total-Queries-by-Return-Code">
+<?php
+	$query = "SELECT `return_value`, count(`return_value`) as `count` FROM `ltsv` where `time` >= $startTime and `time` < $now + $period GROUP BY `return_value` ORDER BY count(`return_value`) DESC";
+	$res = mysqli_query($link, $query);
+	while($row = mysqli_fetch_assoc($res))
+		echo $row['return_value']." - ".$row['count']."<br/>\n";
+?>
+  </div>
+  <div class="Total-Queries-by-Server">
+<?php
+	$query = "SELECT `server`, count(`server`) as `count` FROM `ltsv` where `time` >= $startTime and `time` < $now + $period GROUP BY `server` ORDER BY count(`server`) DESC";
+	$res = mysqli_query($link, $query);
+	while($row = mysqli_fetch_assoc($res))
+		echo $row['server']." - ".$row['count']."<br/>\n";
+?>
+  </div>
+  <div class="Total-Queries-by-Src-IP">
+<?php
+	$query = "SELECT `host`, count(`host`) as `count` FROM `ltsv` where `time` >= $startTime and `time` < $now + $period GROUP BY `host` ORDER BY count(`host`) DESC";
+	$res = mysqli_query($link, $query);
+	while($row = mysqli_fetch_assoc($res))
+		echo $row['host']." - ".$row['count']."<br/>\n";
+?>
+  </div>
+  <div class="Total-Queries-by-Type">
+<?php
+	$query = "SELECT `type`, count(`type`) as `count` FROM `ltsv` where `time` >= $startTime and `time` < $now + $period GROUP BY `type` ORDER BY count(`type`) DESC";
+	$res = mysqli_query($link, $query);
+	while($row = mysqli_fetch_assoc($res))
+		echo $row['type']." - ".$row['count']."<br/>\n";
+?>
+  </div>
+</div>
+
     <script>
 	var ctx = document.getElementById('myChart').getContext('2d');
       var myChart = new Chart(ctx, {
@@ -62,7 +127,7 @@
                         radius: 2
 		    }
 		},
-		responsive: true,
+		responsive: false,
           },
           data: {
             labels: [<?=$delem?>],
@@ -85,41 +150,5 @@
           },
         });
     </script>
-
-<?php
-	$query = "SELECT count(`time`) as `count` FROM `ltsv` where `time` >= $startTime and `time` < $now + $period";
-	$res = mysqli_query($link, $query);
-	$queries = mysqli_fetch_assoc($res)['count'];
-	echo "$queries Queries";
-?>
-
-<?php
-	$query = "SELECT `return_value`, count(`return_value`) as `count` FROM `ltsv` where `time` >= $startTime and `time` < $now + $period GROUP BY `return_value` ORDER BY count(`return_value`) DESC";
-	$res = mysqli_query($link, $query);
-	while($row = mysqli_fetch_assoc($res))
-		echo $row['return_value']." - ".$row['count']."\n";
-?>
-
-<?php
-	$query = "SELECT `host`, count(`host`) as `count` FROM `ltsv` where `time` >= $startTime and `time` < $now + $period GROUP BY `host` ORDER BY count(`host`) DESC";
-	$res = mysqli_query($link, $query);
-	while($row = mysqli_fetch_assoc($res))
-		echo $row['host']." - ".$row['count']."\n";
-?>
-
-<?php
-	$query = "SELECT `server`, count(`server`) as `count` FROM `ltsv` where `time` >= $startTime and `time` < $now + $period GROUP BY `server` ORDER BY count(`server`) DESC";
-	$res = mysqli_query($link, $query);
-	while($row = mysqli_fetch_assoc($res))
-		echo $row['server']." - ".$row['count']."\n";
-?>
-
-<?php
-	$query = "SELECT `type`, count(`type`) as `count` FROM `ltsv` where `time` >= $startTime and `time` < $now + $period GROUP BY `type` ORDER BY count(`type`) DESC";
-	$res = mysqli_query($link, $query);
-	while($row = mysqli_fetch_assoc($res))
-		echo $row['type']." - ".$row['count']."\n";
-?>
-
   </body>
 </html>
