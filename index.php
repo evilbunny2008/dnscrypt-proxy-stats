f18ee1c9fe6cc0f08efa6ee1e3a670b67fd26b71
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2021-06-06 23:06:31 +1000

    tweaks

--- index.php
+++ index.php
@@ -48,92 +48,84 @@
 		$cached .= ", ".mysqli_fetch_assoc($res)['count'];
 	}
 ?>
-
 <div class="grid-container">
   <div class="DNS-Hits" style='width:100%;height:100%'><canvas id="myChart" style='width:100%;height:100%'></canvas></div>
-  <div class="Total-Queries-by-Return-Code">
-<br/>
-<table class="blueTable">
-<thead>
-<tr>
-<th>Return</th>
-<th>Value</th>
-</tr>
-</thead>
-<tbody>
+  <div class="Total-Queries-by-Return-Code" style="position: relative; height:23vh; width:23vw"><canvas id="rcChart"></canvas></div>
 <?php
+	$rclabels = $rcdata = "";
 	$query = "SELECT `return_value`, count(`return_value`) as `count` FROM `ltsv` where `time` >= $startTime and `time` < $now + $period GROUP BY `return_value` ORDER BY count(`return_value`) DESC";
 	$res = mysqli_query($link, $query);
 	while($row = mysqli_fetch_assoc($res))
-		echo "<tr><td>${row['return_value']}</td><td>${row['count']}</td></tr>\n";
+	{
+		if($rclabels != "")
+			$rclabels .= ", ";
+		if($rcdata != "")
+			$rcdata .= ", ";
+
+		$rclabels .= '"'.$row['return_value'].'"';
+		$rcdata .= $row['count'];
+	}
 ?>
-</tbody>
-</tr>
-</table>
-  </div>
   <div class="Total-Queries-by-Server">
-<br/>
-<table class="blueTable">
-<thead>
-<tr>
-<th>Server</th>
-<th>Value</th>
-</tr>
-</thead>
-<tbody>
+  <div class="Total-Queries-by-Server" style="position: relative; height:23vh; width:23vw"><canvas id="serverChart"></canvas></div>
 <?php
+	$serverlabels = $serverdata = "";
 	$query = "SELECT `server`, count(`server`) as `count` FROM `ltsv` where `time` >= $startTime and `time` < $now + $period GROUP BY `server` ORDER BY count(`server`) DESC";
 	$res = mysqli_query($link, $query);
 	while($row = mysqli_fetch_assoc($res))
-		echo "<tr><td>${row['server']}</td><td>${row['count']}</td></tr>\n";
+	{
+		if($serverlabels != "")
+			$serverlabels .= ", ";
+		if($serverdata != "")
+			$serverdata .= ", ";
+
+		if($row['server'] == '-')
+			$row['server'] = "From Cache";
+
+		$serverlabels .= '"'.$row['server'].'"';
+		$serverdata .= $row['count'];
+	}
 ?>
-</tbody>
-</tr>
-</table>
   </div>
-  <div class="Total-Queries-by-Src-IP">
-<br/>
-<table class="blueTable">
-<thead>
-<tr>
-<th>Source IP</th>
-<th>Value</th>
-</tr>
-</thead>
-<tbody>
+  <div class="Total-Queries-by-Src=IP" style="position: relative; height:23vh; width:23vw"><canvas id="srcIPChart"></canvas></div>
 <?php
+	$srciplabels = $srcipdata = "";
 	$query = "SELECT `host`, count(`host`) as `count` FROM `ltsv` where `time` >= $startTime and `time` < $now + $period GROUP BY `host` ORDER BY count(`host`) DESC";
 	$res = mysqli_query($link, $query);
 	while($row = mysqli_fetch_assoc($res))
-		echo "<tr><td>${row['host']}</td><td>${row['count']}</td></tr>\n";
+	{
+		if($srciplabels != "")
+			$srciplabels .= ", ";
+		if($srcipdata != "")
+			$srcipdata .= ", ";
+
+		$srciplabels .= '"'.$row['host'].'"';
+		$srcipdata .= $row['count'];
+	}
 ?>
-</tbody>
-</tr>
-</table>
-  </div>
-  <div class="Total-Queries-by-Type"><canvas id="typeChart"></canvas></div>
+  <div class="Total-Queries-by-Type" style="position: relative; height:23vh; width:23vw"><canvas id="typeChart"></canvas></div>
 <?php
-	$donutlabels = $donutdata = "";
+	$typelabels = $typedata = "";
 	$query = "SELECT `type`, count(`type`) as `count` FROM `ltsv` where `time` >= $startTime and `time` < $now + $period GROUP BY `type` ORDER BY count(`type`) DESC";
 	$res = mysqli_query($link, $query);
 	while($row = mysqli_fetch_assoc($res))
 	{
-		if($donutlabels != "")
-			$donutlabels .= ", ";
-		if($donutdata != "")
-			$donutdata .= ", ";
+		if($typelabels != "")
+			$typelabels .= ", ";
+		if($typedata != "")
+			$typedata .= ", ";
 
-		$donutlabels .= '"'.$row['type'].'"';
-		$donutdata .= $row['count'];
+		$typelabels .= '"'.$row['type'].'"';
+		$typedata .= $row['count'];
 	}
 ?>
 </div>
 
     <script>
-      var donutData = {
-	labels: [ <?=$donutlabels?> ],
+      var rcData = {
+	labels: [ <?=$rclabels?> ],
 	datasets: [{
-	    data: [ <?=$donutdata?> ],
+	    data: [ <?=$rcdata?> ],
 	    backgroundColor: [
                 "#039741",
                 "#3498db",
@@ -146,14 +138,129 @@
             ],
 	}],
       };
-      var donut = document.getElementById('typeChart').getContext('2d');
-      var myDonut = new Chart(donut, {
+      var rc = document.getElementById('rcChart').getContext('2d');
+      var myrc = new Chart(rc, {
 	type: 'doughnut',
-	data: donutData,
+	data: rcData,
 	options: {
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Return Codes',
+                    },
+                },
 	    responsive: true,
 	},
       });
+      myrc.resize(100, 100);
+
+      var serverData = {
+	labels: [ <?=$serverlabels?> ],
+	datasets: [{
+	    data: [ <?=$serverdata?> ],
+	    backgroundColor: [
+                "#039741",
+                "#3498db",
+                "#e83e8c",
+                "#375a7f",
+                "#00bc8c",
+		"#fae372",
+		"#333333",
+		"#00aabb",
+            ],
+	}],
+      };
+      var server = document.getElementById('serverChart').getContext('2d');
+      var myserver = new Chart(server, {
+	type: 'doughnut',
+	data: serverData,
+	options: {
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Requests by Server',
+                    },
+                },
+	    responsive: true,
+	},
+      });
+      myserver.resize(100, 100);
+
+      var srcipData = {
+	labels: [ <?=$srciplabels?> ],
+	datasets: [{
+	    data: [ <?=$srcipdata?> ],
+	    backgroundColor: [
+                "#039741",
+                "#3498db",
+                "#e83e8c",
+                "#375a7f",
+                "#00bc8c",
+		"#fae372",
+		"#333333",
+		"#00aabb",
+            ],
+	}],
+      };
+      var srcip = document.getElementById('srcIPChart').getContext('2d');
+      var mysrcip = new Chart(srcip, {
+	type: 'doughnut',
+	data: srcipData,
+	options: {
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Src IP Requests',
+                    },
+                },
+	    responsive: true,
+	},
+      });
+      mysrcip.resize(100, 100);
+
+      var typeData = {
+	labels: [ <?=$typelabels?> ],
+	datasets: [{
+	    data: [ <?=$typedata?> ],
+	    backgroundColor: [
+                "#039741",
+                "#3498db",
+                "#e83e8c",
+                "#375a7f",
+                "#00bc8c",
+		"#fae372",
+		"#333333",
+		"#00aabb",
+            ],
+	}],
+      };
+      var typeid = document.getElementById('typeChart').getContext('2d');
+      var mytype = new Chart(typeid, {
+	type: 'doughnut',
+	data: typeData,
+	options: {
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'DNS Type Requests',
+                    },
+                },
+	    responsive: true,
+	},
+      });
+      mytype.resize(100, 100);
 
       var ctx = document.getElementById('myChart').getContext('2d');
       var myChart = new Chart(ctx, {
